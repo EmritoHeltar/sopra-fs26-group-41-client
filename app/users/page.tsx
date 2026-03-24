b9111f557b3cf2e3a2ab7412883bf549b17a18af
Flynn <flynnaustinsinan.diener@uzh.ch>
2026-03-24 10:25:27 +0100

    Started working on #3

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,6 @@
 // this code is part of S2 to display a list of all registered users
 // clicking on a user in this list will display /app/users/[id]/page.tsx
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
-
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useApi } from "@/hooks/useApi";
