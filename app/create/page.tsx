57a02f129036987791907dc1dfbb7ab0001bc668
fly-die <flynn.diener@gmail.com>
2026-04-07 14:16:58 +0200

    Styling and button #11

---
+++ app/create/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+// For components that need React hooks and browser APIs,
+// SSR (server side rendering) has to be disabled.
+// Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+
+import React from "react";
+
+const Profile: React.FC = () => {
+  return (
+    <div className="card-container">
+      <p>
+        <strong>Create Group</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
--- app/users/me/page.tsx
+++ app/users/me/page.tsx
@@ -122,6 +122,10 @@ const Profile: React.FC = () => {
     }
   };
 
+  const handleOpenCreateGroup = () => {
+    router.push("/create");
+  };
+
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -229,9 +233,11 @@ const Profile: React.FC = () => {
               onSearch={handleSearch}
               enterButton
             />
-
             <Button className={styles.authButton} onClick={handleLogout}>
               Log out
+            </Button>
+            <Button className={styles.CreateGroupFab} onClick={handleOpenCreateGroup}>
+              +
             </Button>
           </div>
         </div>
@@ -324,7 +330,7 @@ const Profile: React.FC = () => {
             )}
           </div>
         </Card>
-      </div>
+      </div >
 
       {isUploadDialogOpen && (
         <div className={styles.modalOverlay} onClick={handleCloseDialog}>
@@ -397,7 +403,7 @@ const Profile: React.FC = () => {
           </div>
         </div>
       )}
-    </div>
+    </div >
   );
 };
 
